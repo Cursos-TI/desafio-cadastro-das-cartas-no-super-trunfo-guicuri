6ab984c831b6e5be143ec6444d2b465f354e8d77
guicuri <guilhermecuri@hotmail.com>
2025-08-16 21:44:57 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,14 +9,14 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    int  populacao, pontos_turisticos;
-    float  area, pib;
+    int  populacao, pontos_turisticos, populacao2, pontos_turisticos2;
+    float  area, pib, area2, pib2;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
-    printf("Insira os seguintes atributos das cidades: \n");
+    printf("Insira os seguintes atributos das Carta 1: \n");
     printf("População: ");
     scanf("%i", &populacao);
     printf("Pontos turísticos: ");
@@ -26,10 +26,20 @@ int main() {
 	printf("PIB: ");
 	scanf("%f", &pib);
 
+    printf("\nInsira os seguintes atributos das Carta 2: \n");
+    printf("População: ");
+    scanf("%i", &populacao2);
+    printf("Pontos turísticos: ");
+	scanf("%i", &pontos_turisticos2);
+	printf("Area: ");
+	scanf("%f", &area2);
+	printf("PIB: ");
+	scanf("%f", &pib2);
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    printf("\n  Cidade 1: \n Populacão: %i \n Pontos turísticos: %i \n Area: %f \n PIB: %f \n", populacao, pontos_turisticos, area, pib);
-
+    printf("\nCarta 1: \n  Populacão: %i \n  Pontos turísticos: %i \n  Area: %f \n  PIB: %f \n", populacao, pontos_turisticos, area, pib);
+    printf("\nCarta 2: \n  Populacão: %i \n  Pontos turísticos: %i \n  Area: %f \n  PIB: %f \n", populacao2, pontos_turisticos2, area2, pib2);
     return 0;
 }
